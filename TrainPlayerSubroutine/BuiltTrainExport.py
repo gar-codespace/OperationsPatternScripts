efd4250cd3a24b2e5ef5532102c702bfceba2f92
GregRitacco <greg95747@gmail.com>
2022-07-09 10:25:12 -0700

    2.0.0.b1

--- TrainPlayerSubroutine/BuiltTrainExport.py
+++ TrainPlayerSubroutine/BuiltTrainExport.py
@@ -9,8 +9,8 @@ SCRIPT_NAME ='OperationsPatternScripts.TrainPlayerSubroutine.BuiltTrainExport'
 SCRIPT_REV = 20220101
 
 # SCRIPT_DIR = 'OperationsPatternScripts'
-# SCRIPT_DIR = 'OperationsPatternScripts-2.0.0.b1'
-SCRIPT_DIR = 'OperationsPatternScripts-2.0.0.b2'
+SCRIPT_DIR = 'OperationsPatternScripts-2.0.0.b1'
+# SCRIPT_DIR = 'OperationsPatternScripts-2.0.0.b2'
 
 class StandAloneLogging():
     """Called when this script is used by itself"""
