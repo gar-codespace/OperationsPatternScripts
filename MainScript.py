b06e8a9a0d0d2cfc64041bf37764462b673e17fb
GregRitacco <greg95747@gmail.com>
2022-11-01 11:30:34 -0700

    2.0.0.b2

--- MainScript.py
+++ MainScript.py
@@ -16,8 +16,8 @@ from sys import path as sysPath
 from os import path as OS_PATH
 
 # SCRIPT_DIR = 'OperationsPatternScripts'
-SCRIPT_DIR = 'OperationsPatternScripts-2.0.0.b1'
-# SCRIPT_DIR = 'OperationsPatternScripts-2.0.0.b2'
+# SCRIPT_DIR = 'OperationsPatternScripts-2.0.0.b1'
+SCRIPT_DIR = 'OperationsPatternScripts-2.0.0.b2'
 
 PLUGIN_ROOT = OS_PATH.join(jmri.util.FileUtil.getPreferencesPath(), SCRIPT_DIR)
 
